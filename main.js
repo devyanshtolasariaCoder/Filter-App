2fec2025ac568f6dabb5d45ea2dfd492e002dae0
devyanshtolasariaCoder <93969591+devyanshtolasariaCoder@users.noreply.github.com>
2022-11-14 16:42:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(640,480);
+    canvas.position(110,250);
+    video= createCapture(VIDEO);
+    video.hide();
+tint_color = "";
+}
+function draw(){
+}
+function take_snapshot(){
+   save('student_name.png');
+}
+ function filter_tint()
+    {
+        tint_color= document.getElementById("color_name").value;
+    }
+    
